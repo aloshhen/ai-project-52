df10b9e64178e06f500e258848d6b489a9f9b537
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-11 01:28:32 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'coffee-dark': '#6F4E37',
+        'coffee-beige': '#F5F5DC',
+        'coffee-cream': '#FFF8E1',
+        'coffee-brown': '#A0522D',
+        'coffee-mocha': '#8B4513',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
